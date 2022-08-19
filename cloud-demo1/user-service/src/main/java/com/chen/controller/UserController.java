eb85a3bee676581fab6d5a8ddf65b869617904ab
陈运智 <1156028692@qq.com>
2022-08-19 23:12:08 +0800

    nacos配置管理热更新

--- cloud-demo1/user-service/src/main/java/com/chen/controller/UserController.java
+++ cloud-demo1/user-service/src/main/java/com/chen/controller/UserController.java
@@ -4,6 +4,8 @@ import com.chen.dao.pojo.User;
 import com.chen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("user")
+@RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
 public class UserController {
 
     @Autowired
